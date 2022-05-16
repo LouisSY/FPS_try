51806c6a7261d401e3f6ec30f65388f39aa67131
Shuai Yuan <zczlsy6@ucl.ac.uk>
2022-05-16 12:36:11 +0100

    Update Player camera and ground

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,10 +6,12 @@ public class CameraController : MonoBehaviour
 {
     public float mouseSensitivity = 100F;
     public Transform playerPosition;  // current player position
+    public float x_rotation = 0F;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //hide the cursor
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
@@ -17,9 +19,11 @@ public class CameraController : MonoBehaviour
     {
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        x_rotation -= mouseY;
+        x_rotation = Mathf.Clamp(x_rotation, -80F, 80F);   //limit x_rotation
 
         playerPosition.Rotate(Vector3.up * mouseX);  //left and right
-        transform.localRotation = Quaternion.Euler(mouseY, 0F, 0F);
+        transform.localRotation = Quaternion.Euler(x_rotation, 0F, 0F);  //up and down
 
         // Debug.Log("mouseX: " + mouseX);
         // Debug.Log("mouseY: " + mouseY);
